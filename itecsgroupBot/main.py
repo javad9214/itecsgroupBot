59d0f9e8b8abdb3043b9d74ff5fd9501a6604507
MJavad <javadhosseini14@gmail.com>
2023-09-21 18:17:13 +0330

    migrate to PTB v20 init

--- itecsgroupBot/main.py
+++ itecsgroupBot/main.py
@@ -1,17 +1,34 @@
+import logging
+
 import telebot
+
+import asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder , ContextTypes , CommandHandler
+
 from user_input import UserInputHandler
+from config import TOKEN
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
-
-bot = telebot.TeleBot("6010757760:AAFhe2OKrao_2xib-hcGv49L_ha5kyabQsY")
+bot = telebot.TeleBot(TOKEN)
 
 user_input_handler = UserInputHandler(bot)
 
 product = []
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "به ربات گروه آیتکس خوش آمدید!")
+async def start(update: Update , context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text= "به ربات گروه آیتکس خوش آمدید!"
+        )
+
 
 @bot.message_handler(commands=['add_product'])
 def add_product(message):
@@ -24,4 +41,11 @@ def add_product(message):
 
 
 if __name__ == "__main__":
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler('start' , start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
+
     bot.polling(none_stop=True)
